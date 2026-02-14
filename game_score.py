9263056a07fa8d0cfad1c14526f78da28d0de424
iitp-aimlt-2601884 <konchadapraveen@outlook.com>
2026-02-14 18:56:40 +1100

    Add files via upload

---
+++ game_score.py
@@ -0,0 +1,10 @@
+player_name = input("Player: ")
+games_played = int(input("Games palyed: "))
+total_score = int(input("Total score: "))
+
+average_score = total_score / games_played
+
+print(f"Player: {player_name}")
+print(f"Games played: {games_played}")
+print(f"Total score: {total_score}")
+print(f"Average score: {average_score}")
